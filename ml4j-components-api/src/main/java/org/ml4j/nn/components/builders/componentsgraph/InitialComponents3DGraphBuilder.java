7f8163203eaa4312f485d2e3b8ae61c708cc91a3
Michael Lavelle <michael@lavelle.name>
2019-12-18 13:12:21 +0000

    Overriding getComponentChain method in builders

--- ml4j-components-api/src/main/java/org/ml4j/nn/components/builders/componentsgraph/InitialComponents3DGraphBuilder.java
+++ ml4j-components-api/src/main/java/org/ml4j/nn/components/builders/componentsgraph/InitialComponents3DGraphBuilder.java
@@ -1,9 +1,8 @@
 package org.ml4j.nn.components.builders.componentsgraph;
 
-import org.ml4j.nn.components.DirectedComponentChain;
-import org.ml4j.nn.neurons.NeuronsActivation;
+import org.ml4j.nn.components.DefaultDirectedComponentChain;
 
 public interface InitialComponents3DGraphBuilder extends Components3DGraphBuilder<InitialComponents3DGraphBuilder, InitialComponentsGraphBuilder> {
 
-	DirectedComponentChain<NeuronsActivation, ?, ?, ?> getComponentChain();
+	DefaultDirectedComponentChain getComponentChain();
 }
--- ml4j-components-api/src/main/java/org/ml4j/nn/components/builders/componentsgraph/InitialComponentsGraphBuilder.java
+++ ml4j-components-api/src/main/java/org/ml4j/nn/components/builders/componentsgraph/InitialComponentsGraphBuilder.java
@@ -1,10 +1,9 @@
 package org.ml4j.nn.components.builders.componentsgraph;
 
-import org.ml4j.nn.components.DirectedComponentChain;
-import org.ml4j.nn.neurons.NeuronsActivation;
+import org.ml4j.nn.components.DefaultDirectedComponentChain;
 
 public interface InitialComponentsGraphBuilder extends ComponentsGraphBuilder<InitialComponentsGraphBuilder> {
 
-	DirectedComponentChain<NeuronsActivation, ?, ?, ?> getComponentChain();
+	DefaultDirectedComponentChain getComponentChain();
 	
 }
