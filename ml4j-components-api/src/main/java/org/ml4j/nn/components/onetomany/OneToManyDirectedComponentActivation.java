8af598086ba26f051e73672da651e8ef64afe1ea
Michael Lavelle <michael@lavelle.name>
2019-12-19 22:49:22 +0000

    Javadoc

--- ml4j-components-api/src/main/java/org/ml4j/nn/components/onetomany/OneToManyDirectedComponentActivation.java
+++ ml4j-components-api/src/main/java/org/ml4j/nn/components/onetomany/OneToManyDirectedComponentActivation.java
@@ -3,6 +3,11 @@ package org.ml4j.nn.components.onetomany;
 import org.ml4j.nn.components.GenericOneToManyDirectedComponentActivation;
 import org.ml4j.nn.neurons.NeuronsActivation;
 
+/**
+ * Encapsulates the activation from a OneToManyDirectedComponent.
+ * 
+ * @author Michael Lavelle
+ */
 public interface OneToManyDirectedComponentActivation extends GenericOneToManyDirectedComponentActivation<NeuronsActivation> {
 
 }
