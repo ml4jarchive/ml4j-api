dcac91ff1108c63da09bf8b399344d97add3a3d6
Michael Lavelle <michael@lavelle.name>
2019-12-22 19:35:46 +0000

    Changing hierarchy of TrailingActivationFunctionDirectedComponentChain

--- ml4j-components-api/src/main/java/org/ml4j/nn/components/onetone/TrailingActivationFunctionDirectedComponentChain.java
+++ ml4j-components-api/src/main/java/org/ml4j/nn/components/onetone/TrailingActivationFunctionDirectedComponentChain.java
@@ -6,13 +6,13 @@ import org.ml4j.nn.components.DirectedComponentsContext;
 import org.ml4j.nn.components.activationfunctions.DifferentiableActivationFunctionComponent;
 import org.ml4j.nn.neurons.NeuronsActivation;
 
-public interface TrailingActivationFunctionDirectedComponentChain<L extends DefaultChainableDirectedComponent<? extends DefaultChainableDirectedComponentActivation, ?>>
+public interface TrailingActivationFunctionDirectedComponentChain
 		extends DefaultDirectedComponentChain {
 
 	DifferentiableActivationFunctionComponent getFinalComponent();
 
 	@Override
-	TrailingActivationFunctionDirectedComponentChain<L> dup();
+	TrailingActivationFunctionDirectedComponentChain dup();
 
 	@Override
 	List<DefaultChainableDirectedComponent<?, ?>> decompose();
