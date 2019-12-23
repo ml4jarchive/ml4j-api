3a732b310756bd8c52f12fb12a84f58aa54fbb6b
Michael Lavelle <michael@lavelle.name>
2019-12-23 06:49:54 +0000

    Removing unnecessary classes

--- ml4j-synapses-api/src/main/java/org/ml4j/nn/synapses/DirectedSynapsesBipoleGraphActivation.java
+++
@@ -1,9 +0,0 @@
-package org.ml4j.nn.synapses;
-
-import org.ml4j.nn.components.DirectedComponentBipoleGraphActivation;
-import org.ml4j.nn.neurons.NeuronsActivation;
-
-public interface DirectedSynapsesBipoleGraphActivation extends DirectedComponentBipoleGraphActivation<NeuronsActivation> {
-
-
-}
